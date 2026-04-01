378150ae70bbcea29d46ed71fd2587fe8f872273
MoeNegi-Zero <mcmengcongp@gmail.com>
2026-04-01 18:27:54 +0900

    first commit

---
+++ ManboCode/Cards/Basic/ManboAttack.cs
@@ -0,0 +1,37 @@
+using BaseLib.Utils;
+using Godot;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+using MegaCrit.Sts2.Core.ValueProps;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MoeNegiMod.Manbo.Cards;
+
+public class ManboAttack() : ManboCard(1,
+	CardType.Attack, CardRarity.Basic,
+	TargetType.AnyEnemy)
+{
+	protected override HashSet<CardTag> CanonicalTags => [CardTag.Strike];
+	protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(6, ValueProp.Move)];
+
+	protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+		HoverTipFactory.FromPower<DemonFormPower>()
+	];
+
+	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+	{
+		await CommonActions.CardAttack(this, cardPlay.Target).Execute(choiceContext);
+	}
+
+	protected override void OnUpgrade()
+	{
+		DynamicVars.Damage.UpgradeValueBy(2m);
+	}
+}
